7e96c6834c7e57da3c3090a7bf4ff6d35f4120c7
zeid <zeidhassan4@gmail.com>
2024-01-08 11:07:37 +0300

    Complited assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solutions in this file
+//for (let age = 30; age < 40; age++) {
+    //console.log(`I'm ${age} years old. Happy birthday to me!`);
+    //debugger;
+  //}
+  //const gifts = ["teddy bear", "drone", "doll"];
+
+//function wrapGifts(gifts) {
+  //for (let i = 0; i < gifts.length; i++) {
+    //console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    //debugger;
+  //}
+
+  //return gifts;
+//}
+
+//wrapGifts(gifts);
+
+function writeCards(names,eventName){
+    const message = []
+    for (let i=0;i<names.length; i++){
+      message.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return message;
+}
+function countDown(number) {
+    if (number < 1) {
+      console.log("Please provide a positive integer.");
+      return;
+    }
+  
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+}
